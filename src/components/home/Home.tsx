3eed07fefa99b03096a1e8eca16f96e159617c2f
norrell <nick@workhoundapp.com>
2022-06-15 22:13:07 -0500

    Fix home text

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -67,7 +67,7 @@ export default function Home() {
             <div className="px-4 py-5 my-5 text-center">
                 <h1 className="display-5 fw-bold">Find Restaurants</h1>
                 <div className="col-lg-6 mx-auto">
-                    <p className="lead mb-3">Select Restraunt grade to filter by</p>
+                    <p className="lead mb-3">Either search restaurants by name or filter by grade</p>
                     <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
                         <div className="col-lg-8 col-sm-12">
                             <Form.Control type="text" placeholder="Search for a restaurant by name..." onChange={handleInput} value={search} />
